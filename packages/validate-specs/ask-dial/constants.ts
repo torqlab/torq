a33b02fbb71e4c26a6cf54fb0d656258c82d701a
balovbohdan <balovbohdan@gmail.com>
2026-01-13 16:11:20 +0200

    schema-fix

--- packages/validate-specs/ask-dial/constants.ts
+++ packages/validate-specs/ask-dial/constants.ts
@@ -1,5 +1,5 @@
 export const CONFIG = {
-  MODEL: 'gpt-5-nano-2025-08-07',
+  MODEL: 'gpt-5-mini-2025-08-07',
   API_VERSION: '2024-12-01-preview',
   API_KEY: String(process.env.DIAL_KEY),
   TEMPERATURE: 1,
