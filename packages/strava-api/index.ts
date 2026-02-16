97a549e17f9c8e935d531855eeca0941fca01a97
balovbohdan <balovbohdan@gmail.com>
2026-02-16 12:46:09 +0200

    id:27 feat(visible-prompt): prompt modulle

--- packages/strava-api/index.ts
+++ packages/strava-api/index.ts
@@ -2,6 +2,4 @@ export * from './types';
 export * from './constants';
 export * from './activity';
 export * from './activities';
-export { getStravaAuthUrl, exchangeStravaAuthToken, refreshStravaAuthToken } from './auth';
-export * from './auth/types';
-export * from './auth/constants';
+export * from './auth';
